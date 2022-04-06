474e8a27985302650121bc53c543b2fea123817b
easylearningbd <elebdsoft@gmail.com>
2022-04-06 17:30:14 +0600

    m7. Laravel 8 Multi Auth Part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,5 +7,5 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
-}
+} 
  
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -12,6 +12,12 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
 
+use Illuminate\Contracts\Auth\StatefulGuard;
+use App\Actions\Fortify\AttemptToAuthenticate;
+use App\Actions\Fortify\RedirectIfTwoFactorAuthenticatable;
+use App\Http\Controllers\AdminController;
+use Auth;
+
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -21,7 +27,9 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->when([AdminController::class,AttemptToAuthenticate::class,RedirectIfTwoFactorAuthenticatable::class])->needs(StatefulGuard::class)->give(function (){
+                return Auth::gurad('admin');
+        });
     }
 
     /**
